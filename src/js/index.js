31f1d782a19dbbe65ed47d494d68745b9fcf448e
Joao Paulo <jpamdev@gmail.com>
2022-09-03 22:47:17 -0300

    feat: marsh badge

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,39 @@
 const badges = [
-  `<div class="boulder-badge">
-    <div class="badge part1"></div>
-    <div class="badge part1"></div>
-    <div class="badge part1"></div>
-    <div class="badge part1"></div>
-    </div>
+  `
 
     <p>Boulder Badge</p>`,
 
-  `<div class="boulder-badge">
-    </div>
+  `
 
     <p>Cascate Badge</p>`,
 
-  `<div class="boulder-badge">
-    </div>
+  `
 
     <p>Thunder Badge</p>`,
 
-  `<div class="boulder-badge">
-    </div>
+  `
 
     <p>Rainbow Badge</p>`,
 
-  `<div class="boulder-badge">
-    </div>
+  `
 
     <p>Soul Badge</p>`,
 
-  `<div class="boulder-badge">
+  `<div class="marsh-badge">
+    <div class="badge part1"></div>
+    <div class="badge part2"></div>
+    <div class="badge part3">
+        <div></div>
+    </div>
     </div>
 
     <p>Marsh Badge</p>`,
 
-  `<div class="boulder-badge">
-    </div>
+  `
 
     <p>Volcano Badge</p>`,
 
-  `<div class="boulder-badge">
-    </div>
+  `
 
     <p>Earth Badge</p>`,
 ];
